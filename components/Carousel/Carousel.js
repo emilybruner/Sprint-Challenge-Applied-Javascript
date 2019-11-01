322f929a857cd3b2e81df8b9152c7a9bee5bb590
Emily <bruner.emilym@gmail.com>
2019-11-01 12:28:54 -0400

    started attempting stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,47 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+class Carousel {
+  constructor(carouselElement) {
+    this.carouselElement = carouselElement;
+    this.leftButton = this.carouselElement.querySelector(".left-button");
+    this.rightButton = this.carouselElement.querySelector(".right-button");
+    this.images = this.carouselElement.querySelectorAll("img");
+    this.index = 0;
+    this.leftButton.addEventListener("click", () => this.nextPhotoLeft());
+    this.rightButton.addEventListener("click", () => this.nextPhotoRight());
+  }
+}
+
+// function CarouselComponent(){
+//   //create elements
+//   const caroDiv = document.createElement("div");
+//   const leftButton = document.createElement("div");
+//   const img1 = document.createElement("img");
+//   const img2 = document.createElement("img");
+//   const img3 = document.createElement("img");
+//   const img4 = document.createElement("img");
+//   const rightButton = document.createElement("div");
+
+//   // add classes
+//   caroDiv.classList.add("carousel");
+//   leftButton.classList.add("left-button");
+//   rightButton.classList.add("right-button");
+
+//   // add content
+//   img1.src("./assets/carousel/mountains.jpg")
+//   img2.src("./assets/carousel/computer.jpg")
+//   img3.src("./assets/carousel/trees.jpg")
+//   img4.src("./assets/carousel/turntable.jpg")
+
+//   //append
+//   caroDiv.appendChild(leftButton);
+//   caroDiv.appendChild(img1);
+//   caroDiv.appendChild(img2);
+//   caroDiv.appendChild(img3);
+//   caroDiv.appendChild(img4);
+//   caroDiv.appendChild(img5);
+
+//   return caroDiv;
+// }
